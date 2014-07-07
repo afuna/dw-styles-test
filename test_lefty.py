9095716eb6ba9dcd0a0d59e61bc810a9a38e0bc7
Afuna <fu@dreamwidth.org>
2014-07-07 22:27:10 +0800

    Skip 3-column tests; Lefty doesn't implement these

--- test_lefty.py
+++ test_lefty.py
@@ -1,7 +1,28 @@
 from layout import LayoutTestCase
 from needle.cases import NeedleTestCase
 
+from nose.plugins.skip import Skip, SkipTest
+
 class leftyTestCase(LayoutTestCase, NeedleTestCase):
 
     def setUp(self):
         self.layout = "lefty"
+
+    # lefty doesn't implement 3-column layout type
+    def test_desktop_three_columns_sides(self):
+        raise SkipTest
+
+    def test_desktop_three_columns_left(self):
+        raise SkipTest
+
+    def test_desktop_three_columns_right(self):
+        raise SkipTest
+
+    def test_mobile_three_columns_sides(self):
+        raise SkipTest
+
+    def test_mobile_three_columns_left(self):
+        raise SkipTest
+
+    def test_mobile_three_columns_right(self):
+        raise SkipTest
